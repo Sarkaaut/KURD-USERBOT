6978533619b01cdba212038b52df0184faff9407
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-04-04 21:04:59 +0300

    Update bot.py

--- Zaid/modules/bot/bot.py
+++ Zaid/modules/bot/bot.py
@@ -61,7 +61,7 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
     commands: dict = CMD_HELP[modul_name]
     this_command = f"──「 **Help For {str(modul_name).upper()}** 」──\n\n"
     for x in commands:
-        this_command += f" **** `.{str(x)}`\n   **** `{str(commands[x])}`\n\n"
+        this_command += f" `.{str(🔙)}`\n  `{str(commands[🔜])}`\n\n"
     this_command += ""
     bttn = [
         [InlineKeyboardButton(text="Return", callback_data="reopen")],
