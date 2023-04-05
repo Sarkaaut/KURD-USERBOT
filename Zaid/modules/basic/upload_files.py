eb20b92332114c168a0c7cd68e56dbf8134b881f
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-04-05 12:48:28 +0300

    Update upload_files.py

--- Zaid/modules/basic/upload_files.py
+++ Zaid/modules/basic/upload_files.py
@@ -12,20 +12,20 @@ async def progress_callback(current, total, bot: Client, message: Message):
         await message.edit(f"{humanize.naturalsize(current)} / {humanize.naturalsize(total)}")
 
 
-@Client.on_message(filters.command('upload', '.') & filters.me)
+@Client.on_message(filters.command('بەرزکردنەوە', '.') & filters.me)
 async def upload_helper(bot: Client, message: Message):
     if len(message.command) > 1:
-        await bot.send_document('self', message.command[1], progress=progress_callback, progress_args=(bot, message))
+        await bot.send_document('خود', message.command[1], progress=progress_callback, progress_args=(bot, message))
     else:
-        await message.edit('No path provided.')
+        await message.edit('هیچ ڕێگایەک دابین نەکراوە')
         await asyncio.sleep(3)
 
     await message.delete()
 
 
 add_command_help(
-    "upload",
+    "بەرزکردنەوە",
     [
-        [".upload", "Upload the file to telegram from the given system file path."],
+        ["بەرزکردنەوە", "فایلەکە باربکە بۆ تەلەگرام لە ڕێڕەوی پەڕگەی سیستەمی پێدراوەوە."],
     ],
 )
