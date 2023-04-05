437e0517f4f5f537682c97d615b9ff2bb0e78940
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-04-05 13:02:34 +0300

    Update ud.py

--- Zaid/modules/basic/ud.py
+++ Zaid/modules/basic/ud.py
@@ -11,28 +11,28 @@ def replace_text(text):
     return text.replace('"', "").replace("\\r", "").replace("\\n", "").replace("\\", "")
 
 
-@Client.on_message(filters.me & filters.command(["ud"], "."))
+@Client.on_message(filters.me & filters.command(["پێناسە"], "."))
 async def urban_dictionary(bot, message):
     if len(message.text.split()) == 1:
-        await message.edit("Usage: `ud example`")
+        await message.edit("بەکارهێنان: ud نموونە")
         return
     try:
         text = message.text.split(None, 1)[1]
         response = await AioHttp().get_json(
             f"http://api.urbandictionary.com/v0/define?term={text}"
         )
-        word = response["list"][0]["word"]
-        definition = response["list"][0]["definition"]
-        example = response["list"][0]["example"]
+        word = response["لیست"][0]["ووشە"]
+        definition = response["لیست"][0]["پێناسە"]
+        example = response["لیست"][0]["نموونە"]
         resp = (
-            f"**Text: {replace_text(word)}**\n"
-            f"**Meaning:**\n`{replace_text(definition)}`\n\n"
-            f"**Example:**\n`{replace_text(example)}` "
+            f"** دەق: {replace_text(word)}**\n"
+            f"** واتا:**\n`{replace_text(definition)}`\n\n"
+            f"** نموونە:**\n`{replace_text(example)}` "
         )
         await message.edit(resp)
         return
     except Exception as e:
-        await message.edit("`The Urban Dictionary API could not be reached`")
+        await message.edit("نەتوانرا دەست بە API ی فەرهەنگی شارەکان بگات")
         print(e)
         await sleep(3)
         await message.delete()
@@ -40,8 +40,8 @@ async def urban_dictionary(bot, message):
 
 
 add_command_help(
-    "dictionary",
+    "فەرهەنگ",
     [
-        [".ubran | .ud", "Define the word you send or reply to."],
+        ["پێناسە", "پێناسەی ئەو وشەیە بکە کە دەینێری یان وەڵامی دەدەیتەوە"],
     ],
 )
