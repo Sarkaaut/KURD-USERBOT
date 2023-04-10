73627ae088e565a7efbffac3a26e844dd07bcdb9
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-04-10 23:49:56 +0300

    Update bot.py

--- Zaid/modules/bot/bot.py
+++ Zaid/modules/bot/bot.py
@@ -61,7 +61,7 @@ async def on_plug_in_cb(_, callback_query: CallbackQuery):
     commands: dict = CMD_HELP[modul_name]
     this_command = f"**یارمەتی بۆ {str(modul_name).upper()}**\n\n"
     for x in commands:
-        this_command += f" .{str(x)}\n {str(commands[x])}\n\n"
+        this_command += f" `.{str(x)}`\n {str(commands[x])}\n\n"
     this_command += "@SARKAUT"
     bttn = [
         [InlineKeyboardButton(text="گەڕانەوە", callback_data="دووبارە بکەرەوە")],
