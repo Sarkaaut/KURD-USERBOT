37fed9e54ee7171b563a306eeb372f5f4ca292da
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-04-11 23:58:40 +0300

    Update define.py

--- Zaid/modules/basic/define.py
+++ Zaid/modules/basic/define.py
@@ -44,7 +44,7 @@ async def define(bot: Client, message: Message):
     def out_print(word1):
         out = ""
         if "meaning" in list(word1):
-            meaning = word1["meaning"]
+            meaning = word1["واتا"]
             if "noun" in list(meaning):
                 noun = meaning["noun"]
                 out += combine(noun, "noun")
