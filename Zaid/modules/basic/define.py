238fafdbf6260812e316901f2c0856877eaf57e4
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-04-11 23:55:39 +0300

    Update define.py

--- Zaid/modules/basic/define.py
+++ Zaid/modules/basic/define.py
@@ -7,7 +7,7 @@ from Zaid.helper.aiohttp_helper import AioHttp
 from Zaid.modules.help import add_command_help
 
 
-@Client.on_message(filters.command(["پێناسەکردن", "پ"], ".") & filters.me)
+@Client.on_message(filters.command(["define", "dict"], ".") & filters.me)
 async def define(bot: Client, message: Message):
     cmd = message.command
 
@@ -17,7 +17,7 @@ async def define(bot: Client, message: Message):
     elif message.reply_to_message and len(cmd) == 1:
         input_string = message.reply_to_message.text
     elif not message.reply_to_message and len(cmd) == 1:
-        await message.edit("**ناتوانێت تێپەڕێت بۆ بۆشایی**")
+        await message.edit("`Can't pass to the void.`")
         await asyncio.sleep(2)
         await message.delete()
         return
@@ -25,71 +25,71 @@ async def define(bot: Client, message: Message):
     def combine(s_word, name):
         w_word = f"**__{name.title()}__**\n"
         for i in s_word:
-            if "پێناسە" in i:
-                if "نموونە" in i:
+            if "definition" in i:
+                if "example" in i:
                     w_word += (
-                            "\n**پێناسە**\n<pre>"
-                            + i["پێناسە"]
+                            "\n**Definition**\n<pre>"
+                            + i["definition"]
                             + "</pre>\n<b>Example</b>\n<pre>"
-                            + i["نموونە"]
+                            + i["example"]
                             + "</pre>"
                     )
                 else:
                     w_word += (
-                            "\n**پێناسە**\n" + "<pre>" + i["پێناسە"] + "</pre>"
+                            "\n**Definition**\n" + "<pre>" + i["definition"] + "</pre>"
                     )
         w_word += "\n\n"
         return w_word
 
     def out_print(word1):
         out = ""
-        if "واتا" in list(word1):
-            meaning = word1["واتا"]
-            if "ناو" in list(meaning):
-                noun = meaning["ناو"]
-                out += combine(noun, "ناو")
-            if "کردار" in list(meaning):
-                verb = meaning["کردار"]
-                out += combine(verb, "کردار")
-            if "ئامازی پەیوەندی" in list(meaning):
-                preposition = meaning["ئامازی پەیوەندی"]
-                out += combine(preposition, "ئامازی پەیوەندی")
-            if "هاوەڵکار" in list(meaning):
-                adverb = meaning["هاوەڵکار"]
-                out += combine(adverb, "هاوەڵکار")
-            if "ئاوەڵناو" in list(meaning):
-                adjec = meaning["ئاوەڵناو"]
-                out += combine(adjec, ""ئاوەڵناو)
-            if "کورتکراوەی" in list(meaning):
-                abbr = meaning["کورتکراوەی"]
-                out += combine(abbr, "کورتکراوەی")
-            if "هاوارکردن" in list(meaning):
-                exclamation = meaning["هاوارکردن"]
-                out += combine(exclamation, "هاوارکردن")
-            if "کرداری ڕاگوزەر" in list(meaning):
-                transitive_verb = meaning["کرداری ڕاگوزەر"]
-                out += combine(transitive_verb, "کرداری ڕاگوزەر")
-            if "دیاریکەر" in list(meaning):
-                determiner = meaning["دیاریکەر"]
-                out += combine(determiner, "دیاریکەر")
+        if "meaning" in list(word1):
+            meaning = word1["meaning"]
+            if "noun" in list(meaning):
+                noun = meaning["noun"]
+                out += combine(noun, "noun")
+            if "verb" in list(meaning):
+                verb = meaning["verb"]
+                out += combine(verb, "verb")
+            if "preposition" in list(meaning):
+                preposition = meaning["preposition"]
+                out += combine(preposition, "preposition")
+            if "adverb" in list(meaning):
+                adverb = meaning["adverb"]
+                out += combine(adverb, "adverb")
+            if "adjective" in list(meaning):
+                adjec = meaning["adjective"]
+                out += combine(adjec, "adjective")
+            if "abbreviation" in list(meaning):
+                abbr = meaning["abbreviation"]
+                out += combine(abbr, "abbreviation")
+            if "exclamation" in list(meaning):
+                exclamation = meaning["exclamation"]
+                out += combine(exclamation, "exclamation")
+            if "transitive verb" in list(meaning):
+                transitive_verb = meaning["transitive verb"]
+                out += combine(transitive_verb, "transitive verb")
+            if "determiner" in list(meaning):
+                determiner = meaning["determiner"]
+                out += combine(determiner, "determiner")
                 # print(determiner)
             if "crossReference" in list(meaning):
                 crosref = meaning["crossReference"]
                 out += combine(crosref, "crossReference")
-        if "ناونیشان" in list(word1):
+        if "title" in list(word1):
             out += (
-                    "**تێبینی هەڵە**\n\n▪️`"
-                    + word1["ناونیشان"]
+                    "**__Error Note__**\n\n▪️`"
+                    + word1["title"]
                     + "\n\n▪️"
-                    + word1["پەیام"]
+                    + word1["message"]
                     + "\n\n▪️<i>"
-                    + word1["بڕیار"]
+                    + word1["resolution"]
                     + "</i>`"
             )
         return out
 
     if not input_string:
-        await message.edit("وشە داخڵ بکە بۆ گەڕان تگایە ‼️")
+        await message.edit("`Plz enter word to search‼️`")
     else:
         word = input_string
         r_dec = await AioHttp().get_json(
@@ -106,12 +106,12 @@ async def define(bot: Client, message: Message):
                 "`Search result for   `" + f" {v_word}\n\n" + last_output
             )
         else:
-            await message.edit("هیچ ئەنجامێک لە بنکەدراوە نەدۆزراوەتەوە")
+            await message.edit("`No result found from the database.`")
 
 
 add_command_help(
-    "فەرهەنگ",
+    "dictionary",
     [
-        ["پێناسەکردن", "پێناسەی ئەو وشەیە بکە کە دەینێری یان وەڵامی دەدەیتەوە"],
+        [".define | .dict", "Define the word you send or reply to."],
     ],
-)
+) 
