e0c663bb39e8990edcb139c8fcdb26f4caeb77fd
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-04-12 16:25:14 +0300

    Update git_commands.py

--- Zaid/modules/basic/git_commands.py
+++ Zaid/modules/basic/git_commands.py
@@ -60,7 +60,7 @@ async def commit_graph(bot: Client, message: Message):
 
 
 add_command_help(
-    "git",
+    "گیت",
     [
         [".ggraph | .commitgraph", "Gets the commit graph for a Github user."],
     ],
