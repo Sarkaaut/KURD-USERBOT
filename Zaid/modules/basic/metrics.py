97ac5194edc221fa76b95b0be2745ebc14c96861
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-04-18 07:21:24 +0300

    Update metrics.py

--- Zaid/modules/basic/metrics.py
+++ Zaid/modules/basic/metrics.py
@@ -11,16 +11,16 @@ class Custom(dict):
         return 0
 
 
-@Client.on_message(filters.command("wordcount", ".") & filters.me)
+@Client.on_message(filters.command("ژمارەی وشەکان", ".") & filters.me)
 async def word_count(bot: Client, message: Message):
     await message.delete()
     words = Custom()
-    progress = await bot.send_message(message.chat.id, "`Processed 0 messages...`")
+    progress = await bot.send_message(message.chat.id, "** 0 پەیامی پرۆسێس کراوە**")
     total = 0
     async for msg in bot.iter_history(message.chat.id, 1000):
         total += 1
         if total % 100 == 0:
-            await progress.edit_text(f"`Processed {total} messages...`")
+            await progress.edit_text(f"پرۆسێس کراوە{total} پەیامەکان...")
             time.sleep(0.5)
         if msg.text:
             for word in msg.text.split():
@@ -37,12 +37,12 @@ async def word_count(bot: Client, message: Message):
 
 
 add_command_help(
-    "پێوەرەکان",
+    "وشەکان",
     [
         [
-            ".wordcount",
-            "Finds the 25 most used words in the last 1000 messages in a group or private chat. Use in "
-            "chat you want to find the metric in.",
+            "ژمارەی وشەکان",
+            "25 وشەی زۆرترین بەکارهێنراو لە دوایین 1000 نامە لە چاتی گروپ یان تایبەتدا دەدۆزێتەوە. بەکارهێنان لە"
+            "چاتێک کە دەتەوێت پێوەرەکە تێیدا بدۆزیتەوە.",
         ],
     ],
 )
